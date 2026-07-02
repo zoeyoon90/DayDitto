bf3e0e65e75f7d5db3fc450aee509f1c77ebfb76
ZoeYoon <bus1990@naver.com>
2026-07-03 01:14:35 +0900

    feat: landing page 작업

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import NavBar from '@/components/NavBar/NavBar';
+
 
 export const metadata: Metadata = {
   title: 'TwoLog',
@@ -15,8 +15,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
-        <NavBar />
-          <main className="pt-13">{children}</main>
+          {children}
       </body>
     </html>
   );
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,16 @@
+import { Button } from "@/components/Button/Button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <div>
-        홈페이지
-      </div>
-    </>
+    <div className="flex h-dvh flex-col items-center justify-center">
+      <h1 className="text-9xl animate-fade-in  ">TwoLog</h1>
+      <p className="pt-7 text-4xl animate-fade-in text-center leading-12 ">
+        나의 하루를 <br/>다른 언어로 다시 쓰다
+      </p>
+      <Button asChild className="mt-10 animate-fade-in [animation-delay:1s]">
+        <Link href="/login">로그인</Link>
+      </Button>
+    </div>
   );
 }
