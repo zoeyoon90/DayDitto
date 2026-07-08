b33e6023bf65dee4fed6ff8835b5d143a526e27d
ZoeYoon <bus1990@naver.com>
2026-07-09 02:04:59 +0900

    feat: 렌딩 로고 반응형

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex h-dvh flex-col items-center justify-center">
-      <h1 className="text-9xl animate-fade-in  ">DayDitto</h1>
-      <p className="pt-7 text-4xl animate-fade-in text-center leading-12 ">
+      <h1 className="text-5xl sm:text-7xl md:text-9xl animate-fade-in">DayDitto</h1>
+      <p className="pt-4 sm:pt-7 text-xl sm:text-2xl md:text-4xl animate-fade-in text-center leading-8 sm:leading-10 md:leading-12">
         나의 하루를 <br/>다른 언어로 다시 쓰다
       </p>
-      <Button asChild className="mt-10 bg-main animate-fade-in [animation-delay:1s]">
+      <Button asChild className="mt-6 sm:mt-10 bg-main animate-fade-in [animation-delay:1s]">
         <Link href="/login">로그인</Link>
       </Button>
     </div>
