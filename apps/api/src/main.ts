ef9ad7afd65ce48b248f6c765871e77f5bd24c4d
ZoeYoon <theky8690@gmail.com>
2026-09-08 23:31:15 +0900

    fix: origin 허용

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,15 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:5006',
-      'http://localhost:5173',
-      'https://dayditto.co.kr',
-      'https://www.dayditto.co.kr',
-      'https://admin.dayditto.co.kr',
-      'https://dayditto.web.tossmini.com',
-      'https://dayditto.private-web.tossmini.com',
-    ],
+    origin: true,
     credentials: true,
     allowedHeaders: ['Authorization', 'Content-Type'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
